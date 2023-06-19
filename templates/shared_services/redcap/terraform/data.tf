9f4b13595e5e237c1d99fd28160335151c009e53
Tom Young <39765193+t-young31@users.noreply.github.com>
2023-06-19 19:44:28 +0000

    init from admin-vm service

---
+++ templates/shared_services/redcap/terraform/data.tf
@@ -0,0 +1,20 @@
+data "azurerm_subnet" "shared" {
+  resource_group_name  = local.core_resource_group_name
+  virtual_network_name = local.core_vnet
+  name                 = "SharedSubnet"
+}
+
+data "azurerm_subnet" "web_apps" {
+  resource_group_name  = local.core_resource_group_name
+  virtual_network_name = local.core_vnet
+  name                 = "WebAppSubnet"
+}
+
+data "azurerm_key_vault" "keyvault" {
+  name                = local.keyvault_name
+  resource_group_name = local.core_resource_group_name
+}
+
+data "azurerm_resource_group" "rg" {
+  name = local.core_resource_group_name
+}
